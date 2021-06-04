b8968f91fcbbd4ef5d7d95666d4b3e163c6108e8
LeviOS31 <levi.oldescheper@gmail.nl>
2021-06-04 15:52:17 +0200

    nummers

--- datums.php
+++ datums.php
@@ -20,119 +20,119 @@
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">3</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">4</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">5</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">6</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">7</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">8</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">9</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">10</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">11</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">12</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">13</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">14</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">15</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">16</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">17</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">18</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">19</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">20</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">21</label>
                 </div>    
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">22</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">23</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">24</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">25</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">26</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">27</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">28</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">29</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">30</label>
                 </div>
                 <div>
                     <input id="check" type="checkbox" id="datum" name="date" value="1">
-                    <label id="label" for="datum">kan ik hier ook klikken</label>
+                    <label id="label" for="datum">31</label>
                 </div>
             </div>
         </form>
